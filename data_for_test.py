0524b3041d032456857c5f4f766dfd4bee1f0903
Serafima Orlova <eagle.si@inbox.ru>
2026-04-17 23:40:33 +0300

    базовая настройка проекта

---
+++ data_for_test.py
@@ -0,0 +1,62 @@
+import requests
+import random
+import string
+from url import Url
+from endopoints import Endpoints
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    random_string = ''.join(random.choice(letters) for i in range(length))
+    return random_string
+
+class Generator:
+    @staticmethod
+    def generate_random_valid_data():
+        login = generate_random_string(10)
+        password = generate_random_string(10)
+        first_name = generate_random_string(10)
+
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
+        return payload
+    
+    @staticmethod
+    def generate_random_data_wo_login():
+        password = generate_random_string(10)
+        first_name = generate_random_string(10)
+
+        payload = {
+            "login": "",
+            "password": password,
+            "firstName": first_name
+        }
+        return payload
+    
+    @staticmethod
+    def generate_random_data_wo_pasword():
+        login = generate_random_string(10)
+        first_name = generate_random_string(10)
+
+        payload = {
+            "login": login,
+            "password": "",
+            "firstName": first_name
+        }
+        return payload
+    
+class Courier:
+    # функция для регистрации курьера с валидными данными
+    @staticmethod
+    def registration_courier():
+        data = Generator.generate_random_valid_data()
+        response = requests.post(f'{Url.base_url}{Endpoints.create_courier}', data=data)
+        return {"response_text": response.text, "status_code": response.status_code, "data": data}
+    
+    # функция для логина курьера и возвратом id курьера
+    @staticmethod
+    def login_courier_and_get_id(data):
+        response = requests.post(f'{Url.base_url}{Endpoints.login_courier}', data=data)
+        return {"id": str(response.json()["id"]), "response_text": response.text, "status_code": response.status_code}
---
+++ endopoints.py
@@ -0,0 +1,14 @@
+
+class Endpoints:
+    login_courier = '/api/v1/courier/login'
+    create_courier = '/api/v1/courier'
+    delete_courier = '/api/v1/courier/:id'
+
+    get_order_count_courier = '/api/v1/courier/:id/ordersCount'
+
+    finish_order = '/api/v1/orders/finish/:id'
+    cancel_order = '/api/v1/orders/cancel'
+    get_list_order = '/api/v1/orders'
+    get_orders_track = '/api/v1/orders/track'
+    accept_order = '/api/v1/orders/accept/:id'
+    create_order = '/api/v1/orders'
